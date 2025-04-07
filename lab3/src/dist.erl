38f2011eab172c261b90d2782b349b1e0e6bac5f
robert <raniszewskirobert27@gmail.com>
2025-04-07 19:27:24 +0200

    fix fun export

--- lab3/src/dist.erl
+++ lab3/src/dist.erl
@@ -1,6 +1,6 @@
 
 -module(dist).
--export([rand_locs/1, find_closest_parallel/2, find_closest/2]).
+-export([rand_locs/1, find_closest_parallel/2, find_closest/2, find_for_personPID/3]).
 
 rand_locs(N) -> [{rand:uniform(10000), rand:uniform(10000)} ||_ <- lists:seq(1, N)].
 
@@ -15,6 +15,6 @@ find_closest(PeopleLoc, SensorLoc) ->
 find_for_personPID(PersonLoc, SensorsLoc, PID) ->
   PID ! lists:min([{dist(PersonLoc, SL), {PersonLoc, SL}} || SL <- SensorsLoc]).
 
-find_closest_parallel(PeopleLoc, SensorLoc) ->
-  _L1 = [spawn(dist,find_for_personPID, [PL, SensorLoc, self()]) || PL <- PeopleLoc],
+find_closest_parallel(PeopleLoc, SensorsLoc) ->
+  _L1 = [spawn(?MODULE, find_for_personPID, [PL, SensorsLoc, self()]) || PL <- PeopleLoc],
   lists:min([receive Msg -> Msg end || _ <- PeopleLoc]).
